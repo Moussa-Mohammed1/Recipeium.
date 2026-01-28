b59148b8e5dd7961f65c543d1383d0e409394b36
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-01-28 15:48:21 +0100

    feat(model): add Comment model with necessary properties and methods

---
+++ app/Models/Comment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'id_recipe',
+        'id_user'
+    ];
+
+    public function user()
+    {
+        return $this->BelongsTo(User::class, 'id_user');
+    }
+
+    public function recipe()
+    {
+        return $this->belongsTo(Recipe::class, 'id_recipe');
+    }
+
+}
